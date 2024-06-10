9a6908aeff405abbd3747915e5d14a1de8abbb7c
wendell <wendellparanhos326@gmail.com>
2024-06-10 12:18:33 -0300

    Tela de Login Feita!!

---
+++ app/view/JS/index.js
@@ -0,0 +1,12 @@
+const signupButton = document.getElementById('signUp');
+const signinButton = document.getElementById('signIn');
+
+const container = document.getElementById('container');
+
+signupButton.addEventListener('click', () => {
+  container.classList.add("right-panel-active");
+});
+
+signinButton.addEventListener('click', () => {
+  container.classList.remove("right-panel-active");
+});
---
+++ app/view/READ.md
@@ -0,0 +1,1 @@
+Feita tela de view já que terá algumas telas que precisa de html,css e js.
