973a081554254dbca158fdcc10b56a3452efe60f
wendell <wendellparanhos326@gmail.com>
2024-06-10 20:54:19 -0300

    feito tela de login e home

--- app/index.php
+++ app/index.php
@@ -3,44 +3,73 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href="./view/CSS/index.css">
     <title>Login</title>
 </head>
 <body>
 <div class="container" id="container">
+
     <div class="form-container sign-up-container">
-        <form action="" method="post">
+
+        <form action="" method="POST" id="cadastro">
+
             <h1>Criar Usuário</h1>
+
             <span>Use seu email para registrar</span>
+
             <input type="email" name="email" placeholder="Email" required />
+
             <input type="password" name="password" placeholder="Password" required />
+
             <input type="password" name="confirmPassword" placeholder="Confirm Password" required />
-            <button type="submit">Criar</button>
+
+            <button type="submit" form="POST">Criar</button>
         </form>
+
     </div>
 
     <div class="form-container sign-in-container">
-        <form action="" method="post">
+
+        <form action="" method="post" id="login">
+
             <h1>Login</h1>
+
             <span>Use sua conta registrada!</span>
+
             <input type="email" name="email" placeholder="Email" required />
+
             <input type="password" name="password" placeholder="Password" required />
+
             <a href="#">Esqueceu sua senha?</a>
-            <button type="submit">Login</button>
+
+            <button type="submit" form="POST">Login</button>
+
         </form>
+
     </div>
 
     <div class="overlay-container">
         <div class="overlay">
+
             <div class="overlay-panel overlay-left">
+
                 <h1>Bem vindo de volta!</h1>
+
                 <p>Use sua conta para entrar</p>
+
                 <button class="ghost" id="signIn">Login</button>
+
             </div>
+
             <div class="overlay-panel overlay-right">
+
                 <h1>Bem vindo!</h1>
+
                 <p>Crie sua conta para começar</p>
+
                 <button class="ghost" id="signUp">Criar</button>
+
             </div>
         </div>
     </div>
---
+++ app/view/home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="./CSS/home.css">
+    <title>Home</title>
+</head>
+<body>
+
+<nav>
+    <ul>
+        <li><a href="#" class="active"><i class="fas fa-home"></i></a></li>
+        <li><a href="#"><i class="fas fa-user"></i></a></li>
+        <li><a href="#"><i class="fas fa-bell"></i></a></li>
+        <li><a href="#"><i class="fa-solid fa-credit-card"></i></a></li>
+        <li><a href="#"><i class="fa-solid fa-chart-column"></i></a></li>
+        <li><a href="#"><i class="fa-solid fa-gear"></i></a></li>
+    </ul>
+    
+</nav>
+
+<section class="home">
+
+    <div class="container">
+        <h1 class="title">Home</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+    </div>
+
+</section>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="./JS/home.js"></script>
+</body>
+</html>
